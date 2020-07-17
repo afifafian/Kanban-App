b8b4e99dc4a6c58ea9940e6b485d17d29b2670fe
afifafian <afifafian@gmail.com>
2020-07-18 02:00:08 +0700

    +signout google

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -10,6 +10,11 @@ export default {
     methods: {
         logout() {
             localStorage.clear()
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut()
+            .then(function () {
+                console.log('User signed out.');
+            });  
             this.$emit("fromNavbar")
         }
     }

3f7d0a47ae45c0ba92d4acc43e49cf6cf7d08660
afifafian <afifafian@gmail.com>
2020-07-17 22:11:12 +0700

    Added file 'Procfile'

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,9 @@ const routes = require('express').Router();
 const taskRouter = require('./taskRoutes');
 const userRouter = require('./userRoutes');
 
+routes.get('/', function(req, res){
+    res.send('lalala')
+})
 routes.use('/tasks', taskRouter);
 routes.use('/users', userRouter);
 
